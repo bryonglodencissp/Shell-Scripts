6b4e933b3c9ff7f9e732bf88e0b4ff9957b5f748
cbelth <cbelth@purdue.edu>
2016-01-30 12:00:27 -0500

    example shell scripts

---
+++ if_else.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+directory="./Scripting"
+#if statement to check if directory exists
+if [ -d $directory ]; then
+    echo "Directory exists"
+else
+    echo "Directory does not exist"
+fi
+directory="."
+if [ -d $directory ]; then
+    echo "Directory exists"
+else
+    echo "Directory does not exist"
+fi
